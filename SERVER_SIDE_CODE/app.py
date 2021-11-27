63eaae162a01cd83a5c77756d1790d36cc480291
Mithun.S <mithunsrinivasappa@gmail.com>
2021-11-27 13:20:53 +0530

    cors

--- SERVER_SIDE_CODE/app.py
+++ SERVER_SIDE_CODE/app.py
@@ -5,8 +5,19 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import random
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #app.mount("/static", StaticFiles(directory="static"), name="static")
 
