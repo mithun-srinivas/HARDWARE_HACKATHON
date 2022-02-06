7524d525616d4423babdabaf01883a8f6068aadf
Shreevallabha A <60250051+asv0018@users.noreply.github.com>
2022-02-07 00:37:28 +0530

    nothing

---
+++ SERVER_SIDE_CODE/hand_placed.py
@@ -0,0 +1,18 @@
+from websocket import create_connection
+import random
+import time
+import json
+ws = create_connection("ws://192.168.255.48:8000/ws/10950")
+print("Connected to websocket server")
+while True:
+    data = {}
+    data["ecg"] = random.randint(500,991)
+    data["temp"] = round(random.uniform(95,96),2)
+    data["spo2"] = round(random.uniform(87,88),2)
+    data["bpm"] = random.randint(70,71)
+    data["bp"] = random.randint(80,82)
+    ws.send(json.dumps(data))
+    print(json.dumps(data))
+    print("Data is sent")
+    time.sleep(0.08)
+ws.close()
---
+++ SERVER_SIDE_CODE/hand_removed.py
@@ -0,0 +1,18 @@
+from websocket import create_connection
+import random
+import time
+import json
+ws = create_connection("ws://192.168.255.48:8000/ws/10950")
+print("Connected to websocket server")
+while True:
+    data = {}
+    data["ecg"] = random.randint(500,991)
+    data["temp"] = round(random.uniform(77,78),2)
+    data["spo2"] = round(random.uniform(55,57),2)
+    data["bpm"] = random.randint(65,66)
+    data["bp"] = random.randint(65,66)
+    ws.send(json.dumps(data))
+    print(json.dumps(data))
+    print("Data is sent")
+    time.sleep(0.08)
+ws.close()
